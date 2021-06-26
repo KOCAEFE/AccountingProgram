1269f47e11a9cc64d54ea7d6dcb37caf38332cfc
efe <efekocaefe46@gmail.com>
2021-06-26 19:18:08 +0300

    26.06.2021

--- AccountingProgram/ReportForm.cs
+++ AccountingProgram/ReportForm.cs
@@ -21,11 +21,26 @@ namespace AccountingProgram
         SqlCommand command;
         DataTable table;
         SqlDataAdapter da;
-        SqlDataReader sdr;
+        
         private void ReportForm_Load(object sender, EventArgs e)
         {
             comboBox1.SelectedIndex = 0;
-            
+            Siparişler();
+        }
+        
+
+        public void Ciro()
+        {
+            string sorgu = "select * from TotalPrice";
+            dataBase.DataRetrieval(sorgu, dataGridView1);
+            command = new SqlCommand("select sum(totalprice)from TotalPrice where date between'" + dateTimePicker1.Value + "' and '" + dateTimePicker2.Value + "'", dataBase.connection);
+
+
+        }
+
+        public void Siparişler()
+        {
+
             string sorgu = "SELECT O.Id,O.ProductName,O.ProductBarcode,O.Piece,O.SalesPrice,C.CustomerName,C.CustomerLastName,C.Phone,C.City,C.District,C.Address,C.Mail,OrderDetails.Date FROM OrderDetails INNER JOIN Orders o ON OrderDetails.OrderId=o.Id INNER JOIN Customers c ON OrderDetails.CustomerId = c.CustomerId  order by OrderDetails.Date desc";
             command = new SqlCommand(sorgu, dataBase.connection);
             dataBase.connection.Open();
@@ -39,14 +54,35 @@ namespace AccountingProgram
 
         }
 
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                Siparişler();
+                button1.Visible = false;
+            }
+            if (comboBox1.SelectedIndex == 1)
+            {
+                string sorgu = "select * from TotalPrice";
+                dataBase.DataRetrieval(sorgu, dataGridView1);
+                dateTimePicker1.Visible = true;
+                dateTimePicker2.Visible = true;
+                
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-           // command = new SqlCommand("select sum(totalprice)from TotalPrice where date'" + dateTimePicker1.Value + "' Between'" + dateTimePicker1.Value + "'", dataBase.connection);
-            //dataBase.connection.Open();
-            string sorgu = "select sum(totalprice)from TotalPrice where date'" + dateTimePicker1.Value + "' Between'" + dateTimePicker1.Value + "'";
-            dataBase.DataRetrieval(sorgu, dataGridView1);
+
+            dataBase.connection.Open();
+            string cirosorgu = "select sum(totalprice) as Ciro from TotalPrice where date between @tarih1 and @tarih2";
+            table = new DataTable();
+            da = new SqlDataAdapter(cirosorgu, dataBase.connection);
+            da.SelectCommand.Parameters.AddWithValue("@tarih1", dateTimePicker1.Value);
+            da.SelectCommand.Parameters.AddWithValue("@tarih2", dateTimePicker2.Value);
+            da.Fill(table);
+            dataBase.connection.Close();
+            dataGridView1.DataSource = table;
         }
     }
 }
